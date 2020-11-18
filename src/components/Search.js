9adc6c62fa06411a7e9a3db2894e7005d6de88fc
Philippe Paris <contacto@philippeparis.net>
2020-11-18 13:03:31 -0300

    update api movie database

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,7 @@ function SearchMovies() {
     const searchMovies = async(e) => {
         e.preventDefault();
         setSearching(true);
-        const url = `http://www.omdbapi.com/?&apikey=e1a73560&s=${query}&type="movie"`;
+        const url = `http://www.omdbapi.com/?i=tt3896198&apikey=872a863d=${query}&type="movie"`;
         try {
             const response = await fetch(url);
             const data = await response.json();
